b9e2faa7a33f6228b9a229005a74b196c24b813d
Nguyễn Hoàng Thành <92304399+TT1172001@users.noreply.github.com>
2022-07-26 16:15:51 +0700

    Add files via upload

---
+++ StudentCrud/src/main/java/com/example/StudentCrud/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.example.StudentCrud.repository;
+
+import com.example.StudentCrud.domain.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
---
+++ StudentCrud/src/main/java/com/example/StudentCrud/service/StudentService.java
@@ -0,0 +1,29 @@
+package com.example.StudentCrud.service;
+
+import com.example.StudentCrud.domain.Student;
+import com.example.StudentCrud.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository repo;
+    public List<Student> listAll() {
+        return repo.findAll();
+    }
+
+    public void save(Student std) {
+        repo.save(std);
+    }
+
+    public Student get(long id) {
+        return repo.findById(id).get();
+    }
+
+    public void delete(long id) {
+        repo.deleteById(id);
+    }
+}
